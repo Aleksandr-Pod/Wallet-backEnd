78d312c1fe7d6a21a198703a9148723a4b278d28
Aleksandr Podmazko <stdfire@gmail.com>
2022-08-18 21:42:06 +0300

    trash

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose");
 const app = require('./app')
-const path = require("path");
-require("dotenv").config({ path: path.join(__dirname, "./.env") });
+require("dotenv").config();
 const { DB_HOST, PORT = 3030 } = process.env;
+console.log('DB_HOST', DB_HOST);
 
 mongoose.connect(DB_HOST)
   .then(() => app.listen(PORT, () => {
